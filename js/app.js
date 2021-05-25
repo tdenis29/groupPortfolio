b6b4f004ae09678a287ee5a47a83d787741448e0
tdenis29 <tdenis29@outlook.com>
2021-05-25 12:31:26 -0700

    spinnycubenobserver

---
+++ js/app.js
@@ -0,0 +1,33 @@
+// $(window).on('mousemove', function(event) {
+//     var width = $(window).width();
+//     var mouseX = event.pageX - (width * 0.5);
+//     var height = $(window).height();
+//     var mouseY = event.pageY - (height * 0.5);
+//     var xAngle = (mouseY / height) * 90;
+//     var yAngle = (mouseX / width) * 90;
+
+//     $('.cube')[0].style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
+// });
+const faders = document.querySelectorAll('.fade-in');
+
+const appearOptions = {
+  threshold: 0,
+  rootMargin: "0px 0px -150px 0px" 
+};
+
+const appear = new IntersectionObserver
+(function(entries, appear) {
+entries.forEach(entry => {
+    if(!entry.isIntersecting){
+      return
+    } else{
+      entry.target.classList.add('appear');
+      appear.unobserve(entry.target)
+    }
+  });
+}, appearOptions);
+
+
+faders.forEach(fader => {
+  appear.observe(fader);
+});
