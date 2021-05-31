bad869a732243387f296e3eb81dc4bd5177e022f
tdenis29 <tdenis29@outlook.com>
2021-05-31 16:00:44 -0700

    Update app.js

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@
 //     $('.cube')[0].style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
 // });
 
+
 const body = document.getElementById('body');
 document.addEventListener('DOMContentLoaded', e => {
   setTimeout(function(){ 
@@ -22,8 +23,7 @@ const appearOptions = {
   rootMargin: "0px 0px -150px 0px" 
 };
 
-const appear = new IntersectionObserver
-(function(entries, appear) {
+const appear = new IntersectionObserver(function(entries, appear) {
 entries.forEach(entry => {
     if(!entry.isIntersecting){
       return
