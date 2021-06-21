e975832361e35b78b0b4d3fa3d3ab71c1607698f
tdenis29 <tdenis29@outlook.com>
2021-06-21 15:07:14 -0700

    touchcube?

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-$(right).on('mousemove', function(event) {
+$(window).on('mousemove', function(event) {
     var width = $(window).width();
     var mouseX = event.pageX - (width * 1);
     var height = $(window).height();
@@ -9,4 +9,13 @@ $(right).on('mousemove', function(event) {
     $('.cube')[0].style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
 });
 
+$(window).on('touchmove', function(event) {
+  var width = $(window).width();
+  var mouseX = event.pageX - (width * 1);
+  var height = $(window).height();
+  var mouseY = event.pageY - (height * 1);
+  var xAngle = (mouseY / height) * 180;
+  var yAngle = (mouseX / width) * 180;
 
+  $('.cube')[0].style.webkitTransform = "rotateX("+xAngle+"deg) rotateY("+yAngle+"deg)";
+});
