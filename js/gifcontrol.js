9cfc71fe74a7ae876da3dc7dd4ac439205cb0a4d
tdenis29 <tdenis29@outlook.com>
2021-07-02 14:04:58 -0700

    added space to stop email collapse

--- js/gifcontrol.js
+++
@@ -1,6 +0,0 @@
-const mediaQuery = window.matchMedia("(prefers-reduced-motion: reduce))");
-const details = document.querySelector(".gif-con > details");
-console.log(details)
-if (mediaQuery.matches) {
-  details.removeAttribute("open");
-}
